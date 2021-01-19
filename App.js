bd7576ac5fb6809fbdeac88616d0a5b7d9bc6935
nhlong2 <uitoritor@gmail.com>
2021-01-19 10:20:10 +0000

    Add sentry to the project

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+    dsn: 'https://c462fd6f839644b690ada8565b5588f7@o394893.ingest.sentry.io/5598241',
+});
+
+//Sentry.captureException("hello there");
+
+function onPress() {}
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is my first Android app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <Sentry.ErrorBoundary fallback={'An error has occurred'}>
+            <View style={styles.container}>
+                <Text>Text</Text>
+                <Button
+                    onPress={onPress}
+                    title="Button"
+                    color="green"
+                    accessibilityLabel="Learn more about this green button"
+                />
+                <StatusBar style="auto" />
+            </View>
+        </Sentry.ErrorBoundary>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'lightgreen',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
