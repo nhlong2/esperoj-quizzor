0549f70dc21b98ede0bcd77197f4637e2ade2984
nhlong2 <uitoritor@gmail.com>
2021-01-20 13:24:02 +0000

    Add eslint to the project

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'lightgreen',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
     },
