4d92851a2123d9abfdfe5bc30616d3046dd2506f
nhlong2 <uitoritor@gmail.com>
2021-01-23 04:08:16 +0000

    Add React Navigation

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-
 import * as Sentry from '@sentry/react';
 import PouchDB from 'pouchdb';
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 Sentry.init({
   dsn: 'https://c462fd6f839644b690ada8565b5588f7@o394893.ingest.sentry.io/5598241',
@@ -11,23 +12,16 @@ Sentry.init({
 
 //Sentry.captureException("hello there");
 async function get_access_token(api_key) {
-  var url = 'https://iam.cloud.ibm.com/identity/token';
-  var data = `{
-                "grant_type": "urn:ibm:params:oauth:grant-type:apikey",
-                "response_type": "cloud_iam",
-                "apikey": ${api_key}
-            }`;
-  let response = await fetch(
-    url + '?grant_type=urn:ibm:params:oauth:grant-type:apikey&apikey=' + api_key,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Accept: 'application/json',
-      },
-      //body: data // body data type
-    }
-  );
+  var url = 'https://auth.nhlong.workers.dev/';
+  let response = await fetch(url, {
+    method: 'POST',
+    credentials: 'omit',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Accept: 'application/json',
+      api_key: `${api_key}`,
+    },
+  });
   if (response.status == 200) {
     return (await response.json()).access_token;
   } else {
@@ -41,6 +35,7 @@ async function connect_database(url, headers, access_token) {
       for (const header in headers) {
         opts.headers.set(header[0], header[1]);
       }
+      opts.credentials = 'omit';
       opts.headers.set('Accept', '*/*');
       opts.headers.set('Accept-Encoding', 'deflate, gzip');
       //      opts.headers.set('Accept', 'application/json');
@@ -56,7 +51,7 @@ async function connect_database(url, headers, access_token) {
       console.log(info);
     })
     .catch((err) => {
-    sentry.captureException(err)
+      Sentry.captureException(err);
       console.log(err);
     });
   return db;
@@ -66,12 +61,12 @@ async function connect_database(url, headers, access_token) {
   while (true) {
     const api_key = prompt('what is your api key'),
       secret = prompt('what is your secret'),
-      access_token = get_access_token(api_key);
-    files_db = await connect_database(
-      'https://eq-cloudant.nhlong.workers.dev/files',
-      [['api_secret', secret]],
-      access_token
-    );
+      access_token = get_access_token(api_key),
+      files_db = await connect_database(
+        'https://eq-cloudant.nhlong.workers.dev/files',
+        [['api_secret', secret]],
+        access_token
+      );
     if (files_db !== null) {
       break;
     }
@@ -80,18 +75,43 @@ async function connect_database(url, headers, access_token) {
 
 function onPress() {}
 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Text</Text>
+      <Button
+        onPress={() => navigation.navigate('New quizzes')}
+        title="All quizzes"
+        color="green"
+        accessibilityLabel="Learn more about this green button"
+      />
+    </View>
+  );
+}
+
+function NewQuiz({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Text</Text>
+      <Button
+        onPress={() => navigation.navigate('New quizzes')}
+        title="New quiz"
+        color="green"
+        accessibilityLabel="Learn more about this green button"
+      />
+    </View>
+  );
+}
+
 export default function App() {
   return (
     <Sentry.ErrorBoundary fallback={'An error has occurred'}>
-      <View style={styles.container}>
-        <Text>Text</Text>
-        <Button
-          onPress={onPress}
-          title="Button"
-          color="green"
-          accessibilityLabel="Learn more about this green button"
-        />
-      </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen}  options={{ title: 'Dashboard' }} />
+          <Stack.Screen name="NewQuiz" component={NewQuiz}  options={{ title: 'Add a new quiz' }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Sentry.ErrorBoundary>
   );
 }
